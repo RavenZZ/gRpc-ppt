2a6cad0c51eac184e802ae1b3060fac96dd15bf6
RavenZZ <ravenzyj@outlook.com>
2017-01-10 18:24:58 +0800

    nodejs demoapp 50%

--- demoapp/nodejs/dynamic/client.js
+++ demoapp/nodejs/dynamic/client.js
@@ -1,22 +1,26 @@
-var protoPath = __dirname + './minglog.proto';
+var protoPath = __dirname + '/minglog.proto';
 
 var grpc = require('grpc');
 var minglog_proto = grpc.load(protoPath).minglogger;
 
+var mlog = require('./code_gen/minglog_pb');
 
-// function main() {
-//     var client = new minglog_proto.LogService('localhost:50051', grpc.credentials.createInsecure());
+console.dir(new mlog());
 
-//     var log = {
-//         Author: "Raven"
-//     };
-//     client.writeLog(log, function (err, response) {
-//         console.dir(response);
-//     });
+function main() {
+
+    var client = new minglog_proto.LogService('localhost:50051', grpc.credentials.createInsecure());
+
+    var log = {
+        developer: 1
+    };
+    client.writeLog(log, function (err, response) {
+        console.dir(response);
+    });
 
 
 
-// }
+}
 
 // main();
 
